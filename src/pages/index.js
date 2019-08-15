eea43e651f8fa4f3d519ed34b3063a9692138e6e
Maxime <maximebarber@gmail.com>
2019-08-15 18:41:25 +0200

    Update contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,16 +55,13 @@ class HomeIndex extends React.Component {
 
                 <div id="main">
 
-                    {/* <section id="one">
+                    <section id="one">
                         <header className="major">
                             <h2>Ipsum lorem dolor aliquam ante commodo<br />
                             magna sed accumsan arcu neque.</h2>
                         </header>
                         <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
-                        <ul className="actions">
-                            <li><a href="#" className="button">Learn More</a></li>
-                        </ul>
-                    </section> */}
+                    </section>
 
                     <section id="two">
                         <h2>Mes tableaux</h2>
